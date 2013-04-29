868456cf1449be8dac4bab68afdf0d38529f12b2
Niko Böckerman <niko.bockerman@gmail.com>
2013-04-29 21:01:10 +0300

    Add new template class for logging objects
    New loggable template class which allows objects to be printed
    to the output through a virtual function toOutput.

--- loggable.h
+++ loggable.h
@@ -3,6 +3,15 @@
 
 #include <string>
 
+template<typename T> class loggable {
+    virtual void toOutput(std::ostream& out) const = 0;
+    friend std::ostream& ::operator<<(std::ostream& out, const T& a)
+    {
+        a.toOutput(out);
+        return out;
+    }
+};
+
 namespace threadlogger {
 
 class Loggable
